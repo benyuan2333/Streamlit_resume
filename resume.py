b5137cd5992d4f209020ac1a0935fe01e68f2903
benyuan2333 <72808452+benyuan2333@users.noreply.github.com>
2024-06-18 21:35:00 +0800

    Update resume.py

--- resume.py
+++ resume.py
@@ -80,9 +80,11 @@ st.markdown("""
                 margin-top: 0px; /* 移动设备 */
             }
         }
-     a.viewerBadge_container__r5tak {
-           display: none;
-        }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
     </style>
     """, unsafe_allow_html=True)
 
