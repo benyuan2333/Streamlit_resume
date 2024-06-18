36a3a8f996b9d3ee6ee8a237a0c76e449668f80b
benyuan2333 <72808452+benyuan2333@users.noreply.github.com>
2024-06-18 21:21:06 +0800

    Update resume.py

--- resume.py
+++ resume.py
@@ -29,7 +29,7 @@ st.set_page_config(
 )
 hide_st_style = """
             <style>
-            a.viewerBadge_container__r5tak.styles_viewerBadge__CvC9N {
+            .__web-inspector-hide-shortcut__, .__web-inspector-hide-shortcut__ *, .__web-inspector-hidebefore-shortcut__::before, .__web-inspector-hideafter-shortcut__::after {
                visibility: hidden !important;
             }
             </style>
